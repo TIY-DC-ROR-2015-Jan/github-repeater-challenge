cba39f8a2435f034e3d3d50a13603dee090fefcc
Katie Walsh <katie.katiewalsh@gmail.com>
2015-02-23 09:26:45 -0500

    WIP Katie

--- repeater.rb
+++ repeater.rb
@@ -5,7 +5,22 @@ require 'sinatra/base'
 #   public Github repositories for that user
 # Your calls to Github don't need to be authenticated, but it
 #   would be a nice bonus to add a mechanism to do so
+
+
+print "Please enter your GitHub token >"
+token = gets.chomp
+
+
 class GithubRepeater < Sinatra::Base
+
+  # def initialize (token)
+  #   @auth = token
+  #   @header = {"Authorization" => "token #{@auth}", "User-Agent" => "kwals"}
+  # end
+  
+  get 'https://api.github.com/users/:username'
+    a = params[:username]
+    a.to_json
 end
 
 GithubRepeater.run!
