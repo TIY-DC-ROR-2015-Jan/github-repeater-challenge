38a52b61f8687f5c1b66b951b11141499005ab5b
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-23 09:36:51 -0500

    Working version, no authentication

--- repeater.rb
+++ repeater.rb
@@ -1,20 +1,20 @@
 require 'sinatra/base'
 require 'httparty'
-require 'pry'
+
 # For now, this is just a simple repeater of the Github API
 # Making a GET request to `/users/:username` should show the
 #   public Github repositories for that user
 # Your calls to Github don't need to be authenticated, but it
 #   would be a nice bonus to add a mechanism to do so
-class GithubRepeater < Sinatra::Base
-include HTTParty
 
-base_uri = 'https://api.github.com/'
+
+class GithubRepeater < Sinatra::Base
+
 
 get "/users/:username" do 
-  binding.pry
   user = params["username"]
-  # HTTParty.get "#{base_uri}/users/#{user}/repos"
+  repos = HTTParty.get "https://api.github.com/users/#{user}/repos"
+  repos.to_json
 end
 
 
