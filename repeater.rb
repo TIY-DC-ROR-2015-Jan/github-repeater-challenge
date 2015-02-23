5f684b7d00e3efc8e8688c227259ec2a1f4979f6
Emily Chu <Emily.H.Chu@gmail.com>
2015-02-23 09:41:50 -0500

    WIP

--- repeater.rb
+++ repeater.rb
@@ -6,6 +6,19 @@ require 'sinatra/base'
 # Your calls to Github don't need to be authenticated, but it
 #   would be a nice bonus to add a mechanism to do so
 class GithubRepeater < Sinatra::Base
+
+  def repos username  
+    # repos = GitHub.get()
+  end
+
+  get '/users/:username' do
+    repos params[:username]
+  end
+
 end
 
 GithubRepeater.run!
+
+
+# curl -i -u "EmilyChu" https://api.github.com/users/EmilyChu
+# curl -i -u "EmilyChu" https://api.github.com/users -d '{login: params["login"]}' 
