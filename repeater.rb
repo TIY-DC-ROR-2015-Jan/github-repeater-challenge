e21545bcac05188bded92cc282de9cc35c4bbecc
James Dabbs <jamesdabbs@gmail.com>
2015-02-23 09:43:52 -0500

    Working proxy

--- repeater.rb
+++ repeater.rb
@@ -5,7 +5,22 @@ require 'sinatra/base'
 #   public Github repositories for that user
 # Your calls to Github don't need to be authenticated, but it
 #   would be a nice bonus to add a mechanism to do so
+def prompt_for_token
+  print "Enter your github token: "
+  gets.chomp
+end
+
+def find_repos username
+  token = prompt_for_token
+  repos = HTTParty.get("https://api.github.com/users/#{username}/repos", headers: {"Authorization" => "token #{token}", "User-Agent" => "anything"})
+  user_repos = repos.map {|x| x["name"]}
+  user_repos
+end
+
 class GithubRepeater < Sinatra::Base
+  get '/users/:username' do
+    find_repos params[:username]
+  end
 end
 
 GithubRepeater.run!
